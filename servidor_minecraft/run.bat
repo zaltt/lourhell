d13ce4c8cf18c1a564d79b425e66850646f6d2e6
Zaltt <nahuelsantoro7@gmail.com>
2025-04-14 17:42:23 +0000

    [🌙] Backup realizado! 2025-04-14

--- servidor_minecraft/run.bat
+++ servidor_minecraft/run.bat
@@ -1,15 +1,7 @@
 @echo off
-REM Add custom JVM arguments (such as RAM allocation) to the user_jvm_args.txt
-
-java -jar forge-1.20.4-49.1.4-shim.jar --onlyCheckJava
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo If you're struggling to fix the error above, ask for help on the forums or Discord mentioned in the readme.
-    goto :exit
-)
-
-REM Add custom program arguments (such as nogui) to the next line before the %* or pass them to this script directly
-java @libraries/net/minecraftforge/forge/1.20.4-49.1.4/win_args.txt %*
-
-:exit
+REM Forge requires a configured set of both JVM and program arguments.
+REM Add custom JVM arguments to the user_jvm_args.txt
+REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
+REM  pass them to this script directly
+java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.4-45.3.3/win_args.txt %*
 pause
--- servidor_minecraft/run.sh
+++ servidor_minecraft/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
-# Add custom JVM arguments (such as RAM allocation) to the user_jvm_args.txt
-
-java -jar forge-1.20.4-49.1.4-shim.jar --onlyCheckJava || exit 1
-
-# Add custom program arguments (such as nogui) to the next line before the "$@" or pass them to this script directly
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.4-49.1.4/unix_args.txt "$@"
+# Forge requires a configured set of both JVM and program arguments.
+# Add custom JVM arguments to the user_jvm_args.txt
+# Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
+#  pass them to this script directly
+java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.4-45.3.3/unix_args.txt "$@"
